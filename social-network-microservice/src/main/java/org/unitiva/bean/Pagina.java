d14641dd83f49c3d3c20e9cfd7cb8a74dfb54dfa
nicolasalviunitiva <166493911+nicolasalviunitiva@users.noreply.github.com>
2024-04-22 12:40:33 +0200

    Feat/3 creazione bean (#5)

---
+++ social-network-microservice/src/main/java/org/unitiva/bean/Pagina.java
@@ -0,0 +1,62 @@
+package org.unitiva.bean;
+
+import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="pagina")
+public class Pagina extends PanacheEntityBase{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name="idpagina")
+    private Long idpagina;
+    
+    @Column(name="nome")
+    private String nome;
+
+    @Column(name="descrizione")
+    private String descrizione;
+
+    @ManyToOne
+    @JoinColumn(name = "idutente")
+    private Utente utente;
+
+    public  Pagina (){}
+
+    public Pagina(String nome, String descrizione) {
+        this.nome = nome;
+        this.descrizione = descrizione;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescrizione() {
+        return descrizione;
+    }
+
+    public void setDescrizione(String descrizione) {
+        this.descrizione = descrizione;
+    }
+
+    @Override
+    public String toString() {
+        return "Pagina [nome=" + nome + ", descrizione=" + descrizione + "]";
+    }
+
+    
+
+}
