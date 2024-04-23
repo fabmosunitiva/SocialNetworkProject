edfd01895722e62eef63214df427c7b530456fe7
fabiomoscariello <fabio.moscariello@unitiva.it>
2024-04-23 09:31:26 +0200

    Squashed commit of the following:
    commit 066558db9d6c3a3b65d7fc4286a4b4e27b02ee1f
    Author: fabiomoscariello <fabio.moscariello@unitiva.it>
    Date:   Tue Apr 23 08:59:47 2024 +0200
    
        fix: fixed mvnw script for linux bash
    
    commit d14641dd83f49c3d3c20e9cfd7cb8a74dfb54dfa
    Author: nicolasalviunitiva <166493911+nicolasalviunitiva@users.noreply.github.com>
    Date:   Mon Apr 22 12:40:33 2024 +0200
    
        Feat/3 creazione bean (#5)

---
+++ social-network-microservice/src/main/java/org/unitiva/bean/Commento.java
@@ -0,0 +1,57 @@
+package org.unitiva.bean;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="commento")
+public class Commento {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name="idcommento")
+    private Long idcommento;
+
+    @Column(name="corpo")
+    private String corpo;
+
+    @ManyToOne
+    @JoinColumn(name = "idutente")
+    private Utente utente;
+
+    @ManyToOne
+    @JoinColumn(name = "idpost")
+    private Post post;
+
+    public Commento (){}
+
+    public Commento(String corpo) {
+        this.corpo = corpo;
+    }
+
+    public Long getIdcommento() {
+        return idcommento;
+    }
+
+    public String getCorpo() {
+        return corpo;
+    }
+
+    public void setCorpo(String corpo) {
+        this.corpo = corpo;
+    }
+
+    @Override
+    public String toString() {
+        return "Commento [corpo=" + corpo + "]";
+    }
+
+    
+
+}
