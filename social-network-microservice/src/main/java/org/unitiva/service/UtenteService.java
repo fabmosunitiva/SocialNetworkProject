55f18ce327e4330ac35f3c2d90cf5e31e2d1c3b0
fabiomoscariello <fabio.moscariello@unitiva.it>
2024-04-24 16:42:16 +0200

    feat: added methods in UtenteService

--- social-network-microservice/src/main/java/org/unitiva/service/UtenteService.java
+++ social-network-microservice/src/main/java/org/unitiva/service/UtenteService.java
@@ -1,6 +1,9 @@
 package org.unitiva.service;
 
+import org.unitiva.bean.Ruolo;
 import org.unitiva.bean.Utente;
+import org.unitiva.dto.UtenteDTO;
+import org.unitiva.repository.RuoloRepository;
 import org.unitiva.repository.UtenteRepository;
 
 import jakarta.enterprise.context.ApplicationScoped;
@@ -12,15 +15,44 @@ public class UtenteService {
     @Inject
     UtenteRepository utenteRepository;
 
-    public void save (Utente utente){
-      utenteRepository.persist(utente);  
+    @Inject
+    RuoloRepository ruoloRepository;
+
+    public void addUtente(UtenteDTO utenteDto){
+        try {
+            Utente utente = createUtente(utenteDto);
+            utenteRepository.createUtente(utente);
+        } catch (Exception e) {
+            throw new Error(e.getMessage());
+        }
     }
 
-    public Utente findById (Long id){
+    
+    public void updateUtente(UtenteDTO utenteDto){
+        try {
+            Utente utente = createUtente(utenteDto);
+            utenteRepository.updateUtente(utente);
+        } catch (Exception e) {
+            throw new Error(e.getMessage());
+        }
+    }
+
+    public Utente retrieveById (Long id){
         return utenteRepository.findById(id);
     }
 
     public void deleteById (Long id){
         utenteRepository.deleteById(id);
+    }
+
+    private Utente createUtente(UtenteDTO utenteDto){
+        Utente utente = new Utente();
+        utente.setNome(utenteDto.getNome());
+        utente.setCognome(utenteDto.getCognome());
+        utente.setDatanascita(utenteDto.getDatanascita());
+        Long idRuolo = utenteDto.getRuolo();
+        Ruolo ruolo = ruoloRepository.retrieveRuoloById(idRuolo);
+        utente.setRuolo(ruolo); 
+        return utente;
     }
 }
