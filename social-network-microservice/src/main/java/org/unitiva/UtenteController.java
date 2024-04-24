cf3e87e43fe4d8418d49a5f6cf8bec34f9fbecc1
fabiomoscariello <fabio.moscariello@unitiva.it>
2024-04-24 16:42:31 +0200

    feat: expose Utente API in UtenteController

---
+++ social-network-microservice/src/main/java/org/unitiva/UtenteController.java
@@ -0,0 +1,95 @@
+package org.unitiva;
+
+import org.unitiva.bean.Utente;
+import org.unitiva.dto.UtenteDTO;
+import org.unitiva.service.UtenteService;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+
+import jakarta.inject.Inject;
+import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.PUT;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.QueryParam;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+
+public class UtenteController {
+    @Inject
+    UtenteService service;
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Path("/Create")
+    @Transactional
+    @Valid
+    public Response createUtente(UtenteDTO utente) throws JsonProcessingException{
+        try{
+            String message = "{\"message\": "+"\"Create eseguita con successo\"}";
+            service.addUtente(utente);
+            return Response.status(Response.Status.OK).type(MediaType.APPLICATION_JSON).entity(message).build();
+        }catch(Exception e){
+            String message = "{\"errorCode\":5,\"message\": "+"\"Errore nell'operazione di creazione\"}";
+            return Response.status(Response.Status.NOT_MODIFIED)
+            .entity(message)
+            .build();
+        }
+    }
+
+    @PUT
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Path("/Update")
+    @Transactional
+    @Valid
+    public Response updateUtente(UtenteDTO utente) throws JsonProcessingException{
+        try{
+            String message = "{\"message\": "+"\"Update eseguita con successo\"}";
+            service.updateUtente(utente);
+            return Response.status(Response.Status.OK).type(MediaType.APPLICATION_JSON).entity(message).build();
+        }catch(Exception e){
+            String message = "{\"errorCode\":5,\"message\": "+"\"Errore nell'operazione di update\"}";
+            return Response.status(Response.Status.NOT_MODIFIED)
+            .entity(message)
+            .build();
+        }
+    }
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Path("/Delete")
+    @Transactional
+    @Valid
+    public Response deleteUtente(Long Id) throws JsonProcessingException{
+        try{
+            service.deleteById(Id);
+            String message = "{\"message\": "+"\"Delete eseguita con successo\"}";
+            return Response.status(Response.Status.OK).type(MediaType.APPLICATION_JSON).entity(message).build();
+        }catch(Exception e){
+            String message = "{\"errorCode\":5,\"message\": "+"\"Errore nell'operazione di deelet\"}";
+            return Response.status(Response.Status.NOT_MODIFIED)
+            .entity(message)
+            .build();
+        }
+    }
+
+    @GET
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Path("/GetUtenteById")
+    @Transactional
+    @Valid
+    public Response getUtenteById(@QueryParam("id") Long Id) throws JsonProcessingException{
+        try{
+            Utente utente = service.retrieveById(Id);
+            return Response.status(Response.Status.OK).type(MediaType.APPLICATION_JSON).entity(utente).build();
+        }catch(Exception e){
+            String message = "{\"errorCode\":5,\"message\": "+"\"Errore nell'operazione di update\"}";
+            return Response.status(Response.Status.NOT_MODIFIED)
+            .entity(message)
+            .build();
+        }
+    }
+}
