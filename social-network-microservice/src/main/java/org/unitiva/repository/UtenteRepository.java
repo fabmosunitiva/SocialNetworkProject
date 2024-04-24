836a0ef602bd1d6c8a069e65372116f5c92197f7
fabiomoscariello <fabio.moscariello@unitiva.it>
2024-04-24 16:41:39 +0200

    refactor: refactored utenteRepository

--- social-network-microservice/src/main/java/org/unitiva/repository/UtenteRepository.java
+++ social-network-microservice/src/main/java/org/unitiva/repository/UtenteRepository.java
@@ -8,6 +8,37 @@ import jakarta.enterprise.context.ApplicationScoped;
 @ApplicationScoped
 public class UtenteRepository implements PanacheRepositoryBase<Utente,Long>{
 
+    public void createUtente(Utente utente){
+        try {
+            persist(utente);  
+        } catch (Exception e) {
+            throw new Error(e.getMessage());
+        }
+    }
 
+    public void updateUtente(Utente utente){
+        try {
+            persist(utente);
+
+        } catch (Exception e) {
+            throw new Error(e.getMessage());
+        }
+    }
+
+    public void deleteUtente(Long id){
+        try {
+            deleteById(id);
+        } catch (Exception e) {
+            throw new Error(e.getMessage());
+        }
+    }
+
+    public Utente retrieveUtenteById(Long id){
+        try {
+            return findById(id);
+        } catch (Exception e) {
+            throw new Error(e.getMessage());
+        }
+    }
 
 }
