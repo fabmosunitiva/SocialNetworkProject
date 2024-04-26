d6a9dd24770d8a90dce632de5990190031bc208a
fabiomoscariello <fabio.moscariello@unitiva.it>
2024-04-26 14:51:14 +0200

    feat: implemented DataAccessException for UtenteRepository

--- social-network-microservice/src/main/java/org/unitiva/repository/UtenteRepository.java
+++ social-network-microservice/src/main/java/org/unitiva/repository/UtenteRepository.java
@@ -1,43 +1,45 @@
 package org.unitiva.repository;
 
 import org.unitiva.bean.Utente;
+import org.unitiva.exception.database.DataAccessException;
 
 import io.quarkus.hibernate.orm.panache.PanacheRepositoryBase;
 import jakarta.enterprise.context.ApplicationScoped;
 
 @ApplicationScoped
-public class UtenteRepository implements PanacheRepositoryBase<Utente,Long>{
+public class UtenteRepository implements PanacheRepositoryBase<Utente, Long> {
 
-    public void createUtente(Utente utente){
+    public Utente createUtente(Utente utente) throws DataAccessException{
         try {
-            persist(utente);  
+            persistAndFlush(utente);
+            return utente;  
         } catch (Exception e) {
-            throw new Error(e.getMessage());
+            throw new DataAccessException("Impossibile eseguire la creazione dell'utente", e.getCause(),false,true);
         }
     }
 
-    public void updateUtente(Utente utente){
+    public void updateUtente(Utente utente) throws DataAccessException {
         try {
             persist(utente);
 
         } catch (Exception e) {
-            throw new Error(e.getMessage());
+            throw new DataAccessException("Impossibile eseguire l'update dell'utente", e.getCause(), false, true);
         }
     }
 
-    public void deleteUtente(Long id){
+    public void deleteUtente(Long id) throws DataAccessException {
         try {
             deleteById(id);
         } catch (Exception e) {
-            throw new Error(e.getMessage());
+            throw new DataAccessException("Impossibile eseguire la delete dell'utente", e.getCause(), false, true);
         }
     }
 
-    public Utente retrieveUtenteById(Long id){
+    public Utente retrieveUtenteById(Long id) throws DataAccessException {
         try {
             return findById(id);
         } catch (Exception e) {
-            throw new Error(e.getMessage());
+            throw new DataAccessException("Impossibile eseguire la retrieve dell'utente", e.getCause(), false, true);
         }
     }
 
