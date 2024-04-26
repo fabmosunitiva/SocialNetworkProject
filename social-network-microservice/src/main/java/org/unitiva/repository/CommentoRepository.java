2228ff3423d4f6a549a03eba372472afc58c477a
nicolaunitiva <nicola.salvi@unitiva.it>
2024-04-26 09:28:51 +0200

    fix: added methods commento service and repository

--- social-network-microservice/src/main/java/org/unitiva/repository/CommentoRepository.java
+++ social-network-microservice/src/main/java/org/unitiva/repository/CommentoRepository.java
@@ -1,15 +1,31 @@
 package org.unitiva.repository;
 
 import org.unitiva.bean.Commento;
+import org.unitiva.dto.CommentoDTO;
 
 import io.quarkus.hibernate.orm.panache.PanacheRepositoryBase;
 import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
 
 @ApplicationScoped
 public class CommentoRepository implements PanacheRepositoryBase<Commento,Long> {
 
-    public void save (Commento commento){
-        persist(commento);
+    @Inject
+    UtenteRepository utenteRepository;
+
+    @Inject
+    PostRepository postRepository;
+
+    public void save (CommentoDTO commentoDTO){
+        try{
+            Commento commento = new Commento();
+            commento.setCorpo(commentoDTO.getCorpo());
+            commento.setPost(postRepository.retrievePostById(commentoDTO.getIdpost()));
+            commento.setUtente(utenteRepository.retrieveUtenteById(commentoDTO.getIdutente()));
+            persist(commento);
+        } catch (Exception e){
+            throw new Error(e.getMessage());
+        }
     }
 
     public Commento findById (Long id){
@@ -24,4 +40,14 @@ public class CommentoRepository implements PanacheRepositoryBase<Commento,Long> 
         }
     }
 
+    public void updateCommento (CommentoDTO commentoDTO ){
+        try{
+            Commento commento = findById(commentoDTO.getIdcommento());
+            commento.setCorpo(commentoDTO.getCorpo());
+            persist(commento);
+
+        } catch (Exception e){
+            throw new Error(e.getMessage());
+        }
+    }
 }
--- social-network-microservice/src/main/java/org/unitiva/service/CommentoService.java
+++ social-network-microservice/src/main/java/org/unitiva/service/CommentoService.java
@@ -3,8 +3,6 @@ package org.unitiva.service;
 import org.unitiva.bean.Commento;
 import org.unitiva.dto.CommentoDTO;
 import org.unitiva.repository.CommentoRepository;
-import org.unitiva.repository.PostRepository;
-import org.unitiva.repository.UtenteRepository;
 
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
@@ -15,19 +13,22 @@ public class CommentoService {
     @Inject
     CommentoRepository commentoRepository;
 
-    @Inject
-    UtenteRepository utenteRepository;
-
-    @Inject
-    PostRepository postRepository;
-
 
     public void newCommento (CommentoDTO commentoDTO){        
-        Commento commento = new Commento();
-        commento.setCorpo(commentoDTO.getCorpo());
-        commento.setPost(postRepository.retrievePostById(commentoDTO.getIdpost()));
-        commento.setUtente(utenteRepository.retrieveUtenteById(commentoDTO.getIdutente()));
-        commentoRepository.save(commento);
+        commentoRepository.save(commentoDTO);
+    }
+
+    public Commento retrieveById (Long id){
+        return commentoRepository.findById(id);
+
+    }
+
+    public void deleteById (Long id){
+        commentoRepository.deleteCommento(id);
+    }
+
+    public void updateCommento (CommentoDTO commentoDTO){
+        commentoRepository.updateCommento(commentoDTO);
     }
 
 }
